21cab38c1fb0b8299f1de3e3d1376b05c114e617
PhamNgocSonTech <phamngocson7a1@gmail.com>
2024-03-19 21:54:31 +0700

    init vue project

---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import Vuex from "vuex";
+
+const storeData = {
+  state: {},
+  mutations: {},
+  actions: {},
+  modules: {},
+};
+
+const store = new Vuex.Store(storeData);
+export default store;
