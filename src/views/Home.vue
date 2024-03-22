a7cab7889e305bc04e00bf96d6793340155716e5
PhamNgocSonTech <phamngocson7a1@gmail.com>
2024-03-22 15:29:07 +0700

    Blog Post component and Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,42 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <BlogPost :post="welcomeScreen" />
+    <BlogPost
+      :post="post"
+      v-for="(post, index) in sampleBlogPost"
+      :key="index"
+    />
   </div>
 </template>
 
 <script>
+import BlogPost from "@/components/BlogPost.vue";
 export default {
   name: "Home",
-  components: {},
+  components: { BlogPost },
+  data() {
+    return {
+      welcomeScreen: {
+        title: "Welcome to SBlog",
+        blogPost:
+          "Weekly blog article with all things programming including HTML, CSS and JS.",
+        welcomeScreen: true,
+        photo: "coding",
+      },
+      sampleBlogPost: [
+        {
+          title: "This is a Filter Title 1",
+          blogHTML: "This is a filter blog post title",
+          blogCoverPhoto: "beautiful-stories",
+        },
+
+        {
+          title: "This is a Filter Title 2",
+          blogHTML: "This is a filter blog post title",
+          blogCoverPhoto: "designed-for-everyone",
+        },
+      ],
+    };
+  },
 };
 </script>
