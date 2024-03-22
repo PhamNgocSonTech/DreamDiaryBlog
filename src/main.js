7997a903a2191be6be30d20ef602a604c26de366
PhamNgocSonTech <phamngocson7a1@gmail.com>
2024-03-22 20:55:34 +0700

    fix svg icon component instead img tag

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/index.js";
 import storeData from "./store/index.js";
-
 const app = createApp(App);
 app.use(router);
 app.use(storeData);
